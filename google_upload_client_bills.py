4f900630892ba1d4d30584611f45e44f52f13929
AbeeraRauf <abeera_rathore@outlook.com>
2022-09-21 01:28:16 +0500

    first commit

---
+++ google_upload_client_bills.py
@@ -0,0 +1,16 @@
+import json
+import requests
+headers = {"Authorization": "Bearer ya29.a0Aa4xrXOA2UpXxmWr7sl2xOTuPvD14J3tk5KgO0ke9k4vkjEu-u6XSxz42HMKby9ll6ZBAqocqxs-DH3x8hz_pqPeAaj99zIab8dDWXdsm0jdYhY0JklkfiHSSZT-hUCP0oMPzxJSzpLfRkd1N-1Dm7yt359xaCgYKATASARMSFQEjDvL9Dc-3L-BV6kDTOZ9u00xEqw0163"}
+para = {
+    "name": "Abeera Bill.pdf",
+    "parents": ["1u3CFCekQqXeyXlDf2I8JCtPeACKxUEw_"]
+}
+files = {
+    'data': ('metadata', json.dumps(para), 'application/json; charset=UTF-8'),
+    'file': open("Customer_Bills/abeera_4_923006450782.pdf", "rb")
+}
+r = requests.post(
+    "https://www.googleapis.com/upload/drive/v3/files?uploadType=multipart",
+    headers=headers,
+    files=files
+)
